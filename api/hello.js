8e0c30e74549cd07c1ea58ef32dca60239411ab7
Tu Nombre <tu@email.com>
2025-10-15 13:30:24 -0600

    test: api/hello edge function

---
+++ api/hello.js
@@ -0,0 +1,6 @@
+﻿export const config = { runtime: "edge" };
+export default async function handler() {
+  return new Response(JSON.stringify({ ok: true, ts: Date.now() }), {
+    headers: { "content-type": "application/json" }
+  });
+}
