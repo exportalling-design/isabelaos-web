588abfeadbafe02608846c8d63fd7e6bf194b313
exportalling-design <exportalling@gmail.com>
2026-01-20 09:00:48 -0600

    Actualizar App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2850,7 +2850,7 @@ function DashboardView() {
             {appViewMode === "video_prompt" && <VideoFromPromptPanel userStatus={userStatus} spendJades={spendJades} />}
             {appViewMode === "img2video" && <Img2VideoPanel userStatus={userStatus} spendJades={spendJades} />}
             {appViewMode === "library" && <LibraryView />}
-            {{appViewMode === "headshot" && <HeadshotPhotoPanel userStatus={userStatus} />}
+            {appViewMode === "headshot" && <HeadshotPhotoPanel userStatus={userStatus} />}
           </div>
         </section>
       </main>
